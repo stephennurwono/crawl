3a6812c8ef253ff38d91632874a3f81293b57959
Stephen Nurwono <Stephen.Nurwono@walmartlabs.com>
2018-10-05 23:25:51 -0700

    outside of main

---
+++ src/main/java/com/cybercode/service/CrawlService.java
@@ -0,0 +1,9 @@
+package com.cybercode.service;
+
+import java.util.Map;
+
+public interface CrawlService {
+
+	public Map<String, Integer> getStatusStatistic();
+
+}
---
+++ src/main/java/com/cybercode/service/CrawlTool.java
@@ -0,0 +1,108 @@
+package com.cybercode.service;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.cybercode.model.Site;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class CrawlTool implements CrawlService {
+	
+	public final String GOOD_STATUS = "200";
+
+	private Site siteLinks;
+	private String source;
+
+	public CrawlTool(String source) {
+		super();
+		this.source = source;
+	}
+
+	public Map<String, Integer> getStatusStatistic() {
+		URL currentUrl = null;
+		HttpURLConnection http = null;
+		String statusCode;
+		Integer counter = null;
+		Map<String, Integer> counters = new HashMap<String, Integer>();
+		try {
+			if ( this.getSiteLinks() !=null) {
+				/*
+				* loops for links
+				*/
+				for (String url : this.getSiteLinks().links) {
+					//System.out.println(url);
+					currentUrl = new URL(url);
+					http = (HttpURLConnection) currentUrl.openConnection();
+					statusCode = String.valueOf(http.getResponseCode());
+					if (!counters.containsKey(statusCode)) {
+						counters.put(statusCode, 1);
+					} else {
+						counter = counters.get(statusCode);
+						counters.put(statusCode, counter + 1);
+					}
+					//System.out.println(url + " status=" + statusCode);
+					currentUrl = null;
+					http = null;
+				}
+				;
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return counters;
+	}
+
+	public void convert() {
+		Site siteLinks = null;
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			siteLinks = mapper.readValue(new URL(this.getSource()), Site.class);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		this.setSiteLinks(siteLinks);
+	}
+
+	public void reportStatistic(Map<String, Integer> pData) {
+		int allRequests = 0;
+		int goodRequest = -1;
+	
+		if (pData != null) {
+			if (pData.containsKey(this.GOOD_STATUS)) {
+				goodRequest = pData.get(GOOD_STATUS).intValue();
+			}
+			for (Map.Entry<String, Integer> entry : pData.entrySet()) {
+				allRequests = allRequests + entry.getValue().intValue();
+				//System.out.println(entry.getKey() + ":" + entry.getValue());
+			}
+		}
+		System.out.println("total number of http requests performed throughout the entire application = "+allRequests);
+		System.out.println("total number of successful requests= "+goodRequest);
+		System.out.println("total number of failed requests= "+(allRequests-goodRequest));
+		
+
+	}
+
+	public Site getSiteLinks() {
+		return siteLinks;
+	}
+
+	public void setSiteLinks(Site siteLinks) {
+		this.siteLinks = siteLinks;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+}
