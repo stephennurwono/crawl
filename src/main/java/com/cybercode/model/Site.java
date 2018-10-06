f01240569f55cfeafe81a34220ccffddab553f92
Stephen Nurwono <Stephen.Nurwono@walmartlabs.com>
2018-10-05 21:25:47 -0700

    initial program

---
+++ src/main/java/com/cybercode/model/Site.java
@@ -0,0 +1,14 @@
+package com.cybercode.model;
+
+public class Site {
+	public String[] links;
+
+	public String[] getLinks() {
+		return links;
+	}
+
+	public void setLinks(String[] links) {
+		this.links = links;
+	}
+
+}
